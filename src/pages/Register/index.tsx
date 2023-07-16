974e8eb9a381967e0454b135a9576148b2960d7b
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-07-16 00:17:19 -0300

    feat: register user

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,5 +1,12 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import styles from "./Register.module.css";
+import { useAuthentication } from "../../hooks/useAuthentication";
+
+interface IUser {
+  displayName: string;
+  email: string;
+  password: string;
+}
 
 export const Register = () => {
   const [displayName, setDisplayName] = useState("");
@@ -8,10 +15,12 @@ export const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const user = {
+    const user: IUser = {
       displayName,
       email,
       password,
@@ -22,8 +31,15 @@ export const Register = () => {
       return;
     }
 
-    console.log(user);
+    const resp = await createUser(user);
+
   };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
 
   return (
     <div className={styles.register}>
@@ -82,7 +98,10 @@ export const Register = () => {
             required
           />
         </label>
-        <input type="submit" value="Criar" className="btn" />
+        {!loading && <input type="submit" value="Criar" className="btn" />}
+        {loading && (
+          <input type="submit" value="Aguarde..." disabled className="btn" />
+        )}
         {error && <p className="error">{error}</p>}
       </form>
     </div>
