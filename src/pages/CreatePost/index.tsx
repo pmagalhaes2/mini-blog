a9270181fa3532e21329552bcd03e7a6da586f28
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-07-17 23:45:04 -0300

    chore: create new routes

---
+++ src/pages/CreatePost/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./CreatePost.module.css";
+
+export const CreatePost = () => {
+  return (
+    <div className="container">
+      <h1>Create post, works!</h1>
+    </div>
+  );
+};
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./Dashboad.module.css";
+
+export const Dashboard = () => {
+  return (
+    <div className="container">
+      <h1>Dashboard, works!</h1>
+    </div>
+  );
+};
